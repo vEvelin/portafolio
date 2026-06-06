7f8bcd5c86e0f6714c81e7a9f7e021ed007b1336
Valeria Evelin Guarachi Cori <63487301+vEvelin@users.noreply.github.com>
2026-06-05 23:30:37 -0400

    Fix indentation in Project component

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -15,7 +15,7 @@ import '../assets/styles/Project.scss';
 
 function Project() {
     return(
-    <div className="projects-container" id="projects">
+    <div className="projects-container" id="projects"> 
         <h1>Personal Projects</h1>
         <div className="projects-grid">
             <div className="project">
