bc6e19b45c8289741607bebb21faecd853e9679c
Valeria Evelin Guarachi Cori <63487301+vEvelin@users.noreply.github.com>
2026-06-07 17:54:10 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Footer.scss'
-
+ 
 function Footer() {
   return (
     <footer>
